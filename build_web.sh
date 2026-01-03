1a398d95f6f18410e77635b36df064d559aa0717
Jakob Garde <jakob@rigsters.com>
2026-01-03 22:04:35 +0100

    add LICENSE and build_web.sh

---
+++ build_web.sh
@@ -0,0 +1,1 @@
+em++ -o web/index.html src/main.cpp -Wall -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -O3 -Isrc -Lweb -sTOTAL_MEMORY=134217728 -sFORCE_FILESYSTEM=1 -sEXPORTED_RUNTIME_METHODS=ccall -sMINIFY_HTML=0 -sUSE_GLFW=3 -sASYNCIFY --shell-file web/minshell.html web/libraylib.web.a -DPLATFORM_WEB --preload-file resources@resources
